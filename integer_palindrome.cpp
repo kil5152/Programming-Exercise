24e32433195002ddf5c6cc5a755db178801bda08
kil5152 <liuwukai@yahoo.com>
2014-01-02 23:14:54 -0800

    Update integer_palindrome.cpp

--- integer_palindrome.cpp
+++ integer_palindrome.cpp
@@ -1,5 +1,23 @@
 // Determine whether an integer is a palindrome. 
 
+// e.g. 1423443
+// reverse: 3
+// num: 142344
+// reverse: 34
+// num: 14234
+// reverse: 344
+// num: 1423
+// reverse: 3443
+// num: 142
+// reverse: 34432
+// num: 14
+// reverse: 344324
+// num: 1
+// reverse: 3443241
+// num: 0
+
+
+
 int reverse(int num)
 {
 	int reverse = 0;
@@ -8,9 +26,10 @@ int reverse(int num)
 		reverse = reverse * 10 + num % 10;
 		num =  num / 10;
 
-		std::cout << "reverse: " << reverse << std::endl;
-		std::cout << "num: " << num << std::endl;
+		// std::cout << "reverse: " << reverse << std::endl;
+		// std::cout << "num: " << num << std::endl;
 	}
 	
 	return reverse;
 }
+
